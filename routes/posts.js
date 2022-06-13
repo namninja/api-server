f19718f18666eaf3f4ad835a32a53d616c4a2fcb
Nam Ninja <namninja@gmail.com>
2022-06-13 16:43:46 -0700

    adding filtered result for Posts endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,27 @@
+const User = require('../data/posts.json')
+
 const postRoutes = (app, fs) => {
     // variables
     const dataPath = './data/posts.json';
   
     // READ
     app.get('/posts', (req, res) => {
+      
       fs.readFile(dataPath, 'utf8', (err, data) => {
         if (err) {
           throw err;
         }
-  
-        res.send(JSON.parse(data));
+        if (!req.query) {
+          res.send(JSON.parse(data));
+        } else {
+          let filteredData = {
+            "posts":User.posts.filter(function (obj) {
+              return obj.userId == req.query.userId
+            })
+          }
+          res.send(filteredData);
+        }
+        
       });
     });
   };
