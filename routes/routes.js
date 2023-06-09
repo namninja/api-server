7f6cdef8df49e72ad62208a80bd1c2b340c3595d
Nam Ninja <namninja@gmail.com>
2023-06-09 15:22:41 -0700

    adding jwt

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,7 @@ const photoRoutes = require('./photos');
 const albumRoutes = require('./albums');
 const productRoutes = require('./products');
 const cvjobRoutes = require('./cvjobs');
+const jwtRoutes = require('./jwt');
 const mobileJwtRoutes = require('./mobilejwt');
 //const client = require('node-iterable-api').create('99a3883fd905413f8ecc4acda85f9a45')
 
