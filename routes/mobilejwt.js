188d8d5240b20dcf8058c4e8f858c8d3fe458d9f
Nam Ninja <namninja@gmail.com>
2023-07-14 10:26:18 -0700

    update exp on JWT

--- routes/mobilejwt.js
+++ routes/mobilejwt.js
@@ -9,7 +9,7 @@ const mobileJwtRoutes = (app, fs) => {
         var time = (Math.floor(new Date().getTime() / 1000))
         console.log(time)
         var token = jwt.sign({
-            exp: time + 60,
+            exp: time + 120,
             iat: time, 
             email: req.body.email
         }, "80774a550ee940cb02cbf6598c942b733ac415cc43169fdde255d9229283046428d5da1b29e71d23d7880def66108a7d07a04fad8e35e47dd72e5a8e085a4a2a");
