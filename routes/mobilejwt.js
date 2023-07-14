6a23d06f311af707f781c0bd489e8072d29e49cc
Nam Ninja <namninja@gmail.com>
2023-07-14 09:36:55 -0700

    update JWT

--- routes/mobilejwt.js
+++ routes/mobilejwt.js
@@ -7,8 +7,8 @@ const mobileJwtRoutes = (app, fs) => {
     app.post('/mobilejwt', (req, res) => {
         console.log(req)
         var token = jwt.sign({
-            exp: 1687982207 + 7689600,
-            iat: 1687982207, 
+            exp: (Date.now / 1000) + 60000,
+            iat: (Date.now / 1000), 
             email: req.body.email
         }, "80774a550ee940cb02cbf6598c942b733ac415cc43169fdde255d9229283046428d5da1b29e71d23d7880def66108a7d07a04fad8e35e47dd72e5a8e085a4a2a");
   
