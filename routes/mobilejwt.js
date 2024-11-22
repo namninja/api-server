021c03fed56ec9a355a4a9811e92f8434fa16603
Nam Ninja <namninja@gmail.com>
2024-11-22 15:48:20 -0800

    update

--- routes/mobilejwt.js
+++ routes/mobilejwt.js
@@ -9,10 +9,10 @@ const mobileJwtRoutes = (app, fs) => {
         var time = (Math.floor(new Date().getTime() / 1000))
         console.log(time)
         var token = jwt.sign({
-            exp: time + 120,
+            exp: time + 600,
             iat: time, 
             email: req.body.email
-        }, "80774a550ee940cb02cbf6598c942b733ac415cc43169fdde255d9229283046428d5da1b29e71d23d7880def66108a7d07a04fad8e35e47dd72e5a8e085a4a2a");
+        }, "8bc52bc88ed097d4bbe28e54d58a35b7fe1d4531a9d1c2273fa0302a5e903674470364107cf878d80984d919d38814f0f0b0e6e04709858213ff8cfd6af0c0f9");
   
         res.status(201).json(token);
         //res.status(201).json({token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTMyNTQ0MzEsImVtYWlsIjoibmFtLm5nbytkcm9pZDRAaXRlcmFibGUuY29tIiwiaWF0IjoxNjg3OTgyMjA3fQ.YHOu1fweJI1F3OhA_tLQewxuSG5-Qv2O7iIhyTkSV-g"})
