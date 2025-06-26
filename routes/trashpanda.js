24d5bca83735471cf3a72214775f8f02578a4787
Nam Ninja <namninja@gmail.com>
2025-06-25 17:49:20 -0700

    adding route

--- routes/trashpanda.js
+++ routes/trashpanda.js
@@ -1,14 +1,16 @@
-const { TRASHPANDA_API_KEY } = require('../config');
+const TRASHPANDA_API_KEY = process.env.TRASHPANDA_API_KEY;
+
 
 const trashpandaRoutes = (app, fs) => {
   app.post('/trashpanda', (req, res) => {
     const apiKey = req.headers['x-api-key'];
 
     // Check if the API key is present and valid
-    if (apiKey !== TRASHPANDA_API_KEY) {
-      console.warn("🛑 Unauthorized access attempt to /trashpanda");
-      return res.status(401).send({ error: 'Unauthorized' });
-    }
+    if (req.headers['x-api-key'] !== TRASHPANDA_API_KEY) {
+       console.warn("🛑 Unauthorized access attempt to /trashpanda");
+  return res.status(401).send({ error: 'Unauthorized' });
+}
+    
 
     console.log("📬 Incoming Webhook Payload:");
     console.log(JSON.stringify(req.body, null, 2));
